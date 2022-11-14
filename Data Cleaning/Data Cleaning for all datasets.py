514db1214ae1f0b0cdb81e38f5064740a80fdd97
Durafshan jawad <112213181+djawad104@users.noreply.github.com>
2022-11-14 11:30:04 -0500

    Update Data Cleaning for all datasets.py

--- Data Cleaning/Data Cleaning for all datasets.py
+++ Data Cleaning/Data Cleaning for all datasets.py
@@ -1,29 +1,37 @@
 import pandas as pd
 
+
 ### BBC News Dataset
+
 
 bbc=pd.read_csv("C:/DEAN/Datasets/bbc.csv")
 bbc.head()
 bbc.describe()
 bbc.columns
 
+
 # checking null values for each column
 bbc.columns.isna().sum()
+
 
 # checking blank values for each column
 print('title = %d' % (bbc['title'].values == '').sum())
 print('url = %d' % (bbc['url'].values == '').sum())
 
+
 # checking tap values for each column
 print('title = %d' % (bbc['title'].values == ' ').sum())
 print('url = %d' % (bbc['url'].values == ' ').sum())
+
 
 # checking 'na' values for each column
 print('title = %d' % (bbc['title'].values == 'na').sum())
 print('url = %d' % (bbc['url'].values == 'na').sum())
 
+
 # checking duplicate values for url column
 print('url = %d' % (bbc['url'].duplicated()).sum())
+
 
 #checking the invalidity of the URLs
 from urllib.request import urlopen, URLError, HTTPError
@@ -42,44 +50,22 @@ url_status = validate_web_url(bbc)
 bbc.insert(0,'url_status', url_status)
 bbc['url_status'].value_counts()
 
-#droping rows that have invalid url
+
+#dropping rows that have invalid url
 bbc.drop(bbc.loc[bbc['url_status']=="invalid"].index, inplace=True)
 
 
 ### ALJAZEERA NEWS DATASETS
 
-# In[ ]:
-
 
 aljazeera=pd.read_csv("C:/DEAN/Datasets/aljazeera.csv")
-
-
-# In[ ]:
-
-
 aljazeera.head()
-
-
-# In[ ]:
-
-
 aljazeera.describe()
-
-
-# In[ ]:
-
-
 aljazeera.columns
-
-
-# In[ ]:
 
 
 # checking null values for each column
 aljazeera.isna().sum()
-
-
-# In[ ]:
 
 
 # checking blank values for each column
@@ -87,15 +73,9 @@ print('title = %d' % (aljazeera['title'].values == '').sum())
 print('url = %d' % (aljazeera['url'].values == '').sum())
 
 
-# In[ ]:
-
-
 # checking tap values for each column
 print('title = %d' % (aljazeera['title'].values == ' ').sum())
 print('url = %d' % (aljazeera['url'].values == ' ').sum())
-
-
-# In[ ]:
 
 
 # checking 'na' values for each column
@@ -103,21 +83,12 @@ print('title = %d' % (aljazeera['title'].values == 'na').sum())
 print('url = %d' % (aljazeera['url'].values == 'na').sum())
 
 
-# In[ ]:
-
-
 # checking duplicate values for url column
 print('url = %d' % (aljazeera['url'].duplicated()).sum())
 
 
-# In[ ]:
-
-
 # dropping rows with duplicate url values
 aljazeera = aljazeera.drop_duplicates(subset=['url'])
-
-
-# In[ ]:
 
 
 ## Checking the URLS invalidity
@@ -126,46 +97,22 @@ aljazeera.insert(0,'url_status', url_status)
 aljazeera['url_status'].value_counts()
 
 
-# In[ ]:
-
-
+#dropping rows that have invalid url
 aljazeera.drop(aljazeera.loc[aljazeera['url_status']=="invalid"].index, inplace=True)
 
 
-# ## CNBC NEWS DATASETS
 
-# In[ ]:
+### CNBC NEWS DATASETS
 
 
 cnbc=pd.read_csv("C:/DEAN/Datasets/cnbc_news_datase.csv")
-
-
-# In[ ]:
-
-
 cnbc.head()
-
-
-# In[ ]:
-
-
 cnbc.describe()
-
-
-# In[ ]:
-
-
 cnbc.columns
-
-
-# In[ ]:
 
 
 # checking null values for each column
 cnbc.isna().sum()
-
-
-# In[ ]:
 
 
 # checking blank values for each column
@@ -173,15 +120,9 @@ print('title = %d' % (cnbc['title'].values == '').sum())
 print('url = %d' % (cnbc['url'].values == '').sum())
 
 
-# In[ ]:
-
-
 # checking tap values for each column
 print('title = %d' % (cnbc['title'].values == ' ').sum())
 print('url = %d' % (cnbc['url'].values == ' ').sum())
-
-
-# In[ ]:
 
 
 # checking 'na' values for each column
@@ -189,14 +130,8 @@ print('title = %d' % (cnbc['title'].values == 'na').sum())
 print('url = %d' % (cnbc['url'].values == 'na').sum())
 
 
-# In[ ]:
-
-
 # checking duplicate values for url column
 print('url = %d' % (cnbc['url'].duplicated()).sum())
-
-
-# In[ ]:
 
 
 ## Checking the URLS invalidity
@@ -205,46 +140,22 @@ cnbc.insert(0,'url_status', url_status)
 cnbc['url_status'].value_counts()
 
 
-# In[ ]:
-
-
+#dropping rows that have invalid url
 cnbc.drop(cnbc.loc[cnbc['url_status']=="invalid"].index, inplace=True)
 
 
-# ## CNN NEWS DATASET
 
-# In[ ]:
+### CNN NEWS DATASET
 
 
 cnn=pd.read_csv("C:/DEAN/Datasets/cnn.csv")
-
-
-# In[ ]:
-
-
 cnn.head()
-
-
-# In[ ]:
-
-
 cnn.describe()
-
-
-# In[ ]:
-
-
 cnn.columns
-
-
-# In[ ]:
 
 
 # checking null values for each column
 cnn.isna().sum()
-
-
-# In[ ]:
 
 
 # checking blank values for each column
@@ -252,15 +163,9 @@ print('title = %d' % (cnn['title'].values == '').sum())
 print('url = %d' % (cnn['url'].values == '').sum())
 
 
-# In[ ]:
-
-
 # checking tap values for each column
 print('title = %d' % (cnn['title'].values == ' ').sum())
 print('url = %d' % (cnn['url'].values == ' ').sum())
-
-
-# In[ ]:
 
 
 # checking 'na' values for each column
@@ -268,21 +173,12 @@ print('title = %d' % (cnn['title'].values == 'na').sum())
 print('url = %d' % (cnn['url'].values == 'na').sum())
 
 
-# In[ ]:
-
-
 # checking duplicate values for url column
 print('url = %d' % (cnn['url'].duplicated()).sum())
 
 
-# In[ ]:
-
-
 # dropping rows with duplicate url values
 cnn = cnn.drop_duplicates(subset=['url'])
-
-
-# In[ ]:
 
 
 ## Checking the URLS invalidity
@@ -291,46 +187,22 @@ cnn.insert(0,'url_status', url_status)
 cnn['url_status'].value_counts()
 
 
-# In[ ]:
-
-
+#dropping rows that have invalid url 
 cnn.drop(cnn.loc[cnn['url_status']=="invalid"].index, inplace=True)
 
 
-# ## JAPAN TIMES NEWS DATASET
 
-# In[ ]:
+### JAPAN TIMES NEWS DATASET
 
 
 japan_times=pd.read_csv("C:/DEAN/Datasets/japan_times.csv")
-
-
-# In[ ]:
-
-
 japan_times.head()
-
-
-# In[ ]:
-
-
 japan_times.describe()
-
-
-# In[ ]:
-
-
 japan_times.columns
-
-
-# In[ ]:
 
 
 # checking null values for each column
 japan_times.isna().sum()
-
-
-# In[ ]:
 
 
 # checking blank values for each column
@@ -338,15 +210,9 @@ print('title = %d' % (japan_times['headline'].values == '').sum())
 print('url = %d' % (japan_times['url'].values == '').sum())
 
 
-# In[ ]:
-
-
 # checking tap values for each column
 print('title = %d' % (japan_times['headline'].values == ' ').sum())
 print('url = %d' % (japan_times['url'].values == ' ').sum())
-
-
-# In[ ]:
 
 
 # checking 'na' values for each column
@@ -354,14 +220,8 @@ print('title = %d' % (japan_times['headline'].values == 'na').sum())
 print('url = %d' % (japan_times['url'].values == 'na').sum())
 
 
-# In[ ]:
-
-
 # checking duplicate values for url column
 print('url = %d' % (japan_times['url'].duplicated()).sum())
-
-
-# In[ ]:
 
 
 ## Checking the URLS invalidity
@@ -370,14 +230,5 @@ japan_times.insert(0,'url_status', url_status)
 japan_times['url_status'].value_counts()
 
 
-# In[ ]:
-
-
+#dropping rows that have invalid url
 japan_times.drop(japan_times.loc[japan_times['url_status']=="invalid"].index, inplace=True)
-
-
-# In[ ]:
-
-
-
-
